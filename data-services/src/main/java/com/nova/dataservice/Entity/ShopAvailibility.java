3b1245c7fe93594ee200d6107159b85209a1427b
kraveendra18 <124889944+kraveendra18@users.noreply.github.com>
2023-05-12 14:42:42 +0530

    FR

---
+++ data-services/src/main/java/com/nova/dataservice/Entity/ShopAvailibility.java
@@ -0,0 +1,42 @@
+package com.nova.dataservice.Entity;
+
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name="shop_availibility")
+public class ShopAvailibility {
+	
+	private Long id;
+	
+	
+	private LocalDateTime fromDate;
+	
+	
+	private LocalDateTime toDate;
+	
+	
+	private LocalTime fromTime;
+	
+	
+	private LocalTime toTime;
+	
+	
+	private LocalTime interval;
+	
+	private Long  shopId;
+	
+	
+	private String status;
+	
+	
+	private String isDeleted;
+	
+	public ShopAvailibility() {
+		
+	}
+
+}
