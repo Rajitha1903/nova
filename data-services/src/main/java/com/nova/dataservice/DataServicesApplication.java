5ae3b9a720b96f2939543622f09e4b336f418a1a
Rajitha1903 <132284151+Rajitha1903@users.noreply.github.com>
2023-05-12 13:08:51 +0530

    Add files via upload

---
+++ data-services/src/main/java/com/nova/dataservice/DataServicesApplication.java
@@ -0,0 +1,13 @@
+package com.nova.dataservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataServicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DataServicesApplication.class, args);
+	}
+
+}
